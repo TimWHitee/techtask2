aa3abb97de669ebfa028115e6f6bfa987f1e4fd9
Тимофей Морозов <tim7.moroz0v@yandex.ru>
2024-05-06 00:28:11 +0300

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static int _min(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    public static int _max(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public static int _sum(int[] numbers) {
+        int sum = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static int _mult(int[] numbers) {
+        int mult = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            mult *= numbers[i];
+        }
+        return mult;
+    }
+
+    public static void main(String[] args) {
+        String fileName = "numbers.txt";
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            String[] numbersAsString = line.split(" ");
+            int[] numbers = new int[numbersAsString.length];
+            for (int i = 0; i < numbersAsString.length; i++) {
+                try {
+                    numbers[i] = Integer.parseInt(numbersAsString[i]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Big number error " + e.getMessage());
+                    System.exit(0);
+                }
+            }
+            bufferedReader.close();
+            fileReader.close();
+            System.out.println(_min(numbers));
+            System.out.println(_max(numbers));
+            System.out.println(_sum(numbers));
+            System.out.println(_mult(numbers));
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
---
+++ MainTest.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+
+public class MainTest {
+    
+    @Test
+    public void Main_tests() {
+        // Массив для тестирования
+        int[] numbers = {5, 3, 9, 1, 7};
+        
+        int result_min = Main._min(numbers);
+        int result_max = Main._max(numbers);
+        int result_mult = Main._mult(numbers);
+        int result_sum = Main._sum(numbers);
+        
+        assertEquals(1, result_min);
+        assertEquals(9, result_max);
+
+        assertEquals(5*3*9*1*7, result_mult);
+        assertEquals(5+3+9+1+7, result_sum);
+    }
+
+    @Test
+    public void speed_test() {
+        int[] numbers = new int[1000000];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100) + 1;
+        }
+        long start = System.currentTimeMillis();
+        int test1 = Main._min(numbers);
+        int test2 = Main._max(numbers);
+        int test3 = Main._mult(numbers);
+        int test4 = Main._sum(numbers);
+
+        long end = System.currentTimeMillis();
+        long time  = (end - start);
+        boolean flag = true ? time <= 25 : false;
+        assertEquals(true, flag);
+    }
+
+    @Test
+    public void negative_numbers_test() {
+        int[] numbers = {-5, -3, -9, -1, -7};
+
+        int result_min = Main._min(numbers);
+        int result_max = Main._max(numbers);
+        int result_mult = Main._mult(numbers);
+        int result_sum = Main._sum(numbers);
+        
+        assertEquals(-9, result_min);
+        assertEquals(-1, result_max);
+
+        assertEquals(-5*3*9*1*7, result_mult);
+        assertEquals(-(5+3+9+1+7), result_sum);
+    }
+}
